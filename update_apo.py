27967100c781c2edfd8f75a2b291d7a26116afe5
Martin Rosales <martinrosalesfg@gmail.com>
2024-11-12 06:01:32 -0500

    Update Script

--- update_apo.py
+++ update_apo.py
@@ -1,12 +1,13 @@
 import requests
 import os
+import re
 
 readme_path = "README.md"
 
 api_key = os.getenv("NASA_API_KEY")
 
 def obtener_apod():
-    url = "https://api.nasa.gov/planetary/apod?api_key={api_key}"
+    url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
     respuesta = requests.get(url)
     
     if respuesta.status_code == 200:
@@ -20,23 +21,31 @@ def obtener_apod():
 imagen_url, title = obtener_apod()
 
 if imagen_url:
-    with open(readme_path, "r") as file:
-        contenido = file.read()
 
-    contenido = contenido.replace(
-        '<img width="500" height="300" src="https://apod.nasa.gov/apod/image/2411/Ngc6888Hoo_Aro_960.jpg" />',
-        f'<img width="500" height="300" src="{imagen_url}" />'
-    )
+    try:
+        with open(readme_path, "r") as file:
+            contenido = file.read()
 
-    contenido = contenido.replace(
-        '<p><i>Title: NGC 6888 - The Crescent Nebula</i></p>',
-        f'<p><i>Title: {title}</i></p>'
-    )
+        contenido = re.sub(
+            r'<img [^>]*src="[^"]*"[^>]*>',  
+            f'<img width="500" height="300" src="{imagen_url}" />',
+            contenido
+        )
+
+        contenido = re.sub(
+            r'<p><i>Title: [^<]*</i></p>',
+            f'<p><i>Title: {title}</i></p>',
+            contenido
+        )
+
+        with open(readme_path, "w") as file:
+            file.write(contenido)
+
+        print("README.md actualizado correctamente.")
+
+    except Exception as e:
+        print(f"Hubo un error al actualizar el README.md: {e}")
 
 
-    with open(readme_path, "w") as file:
-        file.write(contenido)
-
-    print("README.md actualizado correctamente.")
 else:
     print("No se pudo obtener la imagen de la NASA.")
